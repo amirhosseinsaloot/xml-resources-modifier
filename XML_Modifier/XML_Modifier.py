489fd93a862a4d14adb27a548589273cf7daa7de
alefmim <48165275+alefmim@users.noreply.github.com>
2020-06-03 02:51:04 +0430

    optimize the process

--- XML_Modifier/XML_Modifier.py
+++ XML_Modifier/XML_Modifier.py
@@ -1,4 +1,5 @@
 import os
+import re
 import xml.etree.ElementTree as ET
 
 projectPath = ''
@@ -6,8 +7,8 @@ xmlFilePath = ''
 
 
 def getListOfFilesPath(dirName):
-    # create a list of file and sub directories 
-    # names in the given directory 
+    # create a list of file and sub directories
+    # names in the given directory
     listOfFile = os.listdir(dirName)
     allFiles = list()
 
@@ -15,86 +16,117 @@ def getListOfFilesPath(dirName):
     for entry in listOfFile:
         # Create full path
         fullPath = os.path.join(dirName, entry)
-        # If entry is a directory then get the list of files in this directory 
+        # If entry is a directory then get the list of files in this directory
         if os.path.isdir(fullPath):
             allFiles = allFiles + getListOfFilesPath(fullPath)
         else:
-            if entry.endswith(".cs") or entry.endswith(".cshtml") or entry.endswith(".config") :
+            if entry.endswith(".cs") or entry.endswith(
+                    ".cshtml") or entry.endswith(".config"):
                 allFiles.append(fullPath)
-                
+
     return allFiles
 
 
 # return true if text exists in current file
-def searchResourceInFile(filePath,text):
-    with open(filePath,encoding="utf8",errors='ignore') as file:
+def searchResourceInFile(filePath, text):
+    with open(filePath, encoding="utf8", errors='ignore') as file:
         if text in file.read():
             return True
 
 
-
 # convert list to string with new line (for display removed tags in RemovedTags.txt)
-def listToString(list):  
-    
-    # initialize an empty string 
-    str = ""  
-    
-    # traverse in the string   
-    for item in list:  
-        str = str + item + '\n'   
-    
-    # return string   
-    return str
+def listToString(list):
 
+    # initialize an empty string
+    str = ""
+
+    # traverse in the string
+    for item in list:
+        str = str + item + '\n'
+
+    # return string
+    return str
 
 
 fileLists = getListOfFilesPath(projectPath)
 
-# xml.etree.ElementTree instance
-tree = ET.parse(xmlFilePath,ET.XMLParser(encoding="utf-8"))
-root = tree.getroot()
+# # xml.etree.ElementTree instance
+# tree = ET.parse(xmlFilePath, ET.XMLParser(encoding="utf-8"))
+# root = tree.getroot()
 
-# false when specified tag not found in files
-# true  when specified use even one file
-flag = False
+# # false when specified tag not found in files
+# # true  when specified use even one file
+# flag = False
 
-# list of removed tags in xml file
-removedTags = []
-foundTags = []
+# # list of removed tags in xml file
+# removedTags = []
+# foundTags = []
+# counter = 0
+
+# for LocaleResource in root.findall('LocaleResource'):
+#     tagName = LocaleResource.get('Name')
+#     counter = counter + 1
+#     for file in fileLists:
+#         if searchResourceInFile(file,tagName):
+#             flag = True
+#             break
+#         else:
+#             flag = False
+
+#     # flag remains false when specified tag not found in files
+#     if flag is False:
+#         root.remove(LocaleResource)
+#         removedTags.append(tagName)
+#         print(str(counter) + ' : ' + tagName + ' Removed')
+#     else:
+#         foundTags.append(tagName + ' exists in  : ' + file)
+#         print(str(counter) + ' : ' + tagName + ' exists in : '+ file)
+
+usedTags = list()
+foundTags = list()
+removedTags = list()
 counter = 0
 
+for file in fileLists:
+    textfile = open(file, 'r', encoding="utf8", errors='ignore')
+    filetext = textfile.read()
+    textfile.close()
+    matches = re.findall(r'\"(?:[a-zA-Z0-9_()#?.]+\.)*[a-zA-Z0-9_()#?.]+\"',
+                         filetext)
+    usedTags += matches
+
+tree = ET.parse(xmlFilePath, ET.XMLParser(encoding="utf-8"))
+root = tree.getroot()
+
 for LocaleResource in root.findall('LocaleResource'):
-    tagName = LocaleResource.get('Name')
+
+    tagName = '"' + LocaleResource.get('Name') + '"'
     counter = counter + 1
-    for file in fileLists:
-        if searchResourceInFile(file,tagName):
-            flag = True
-            break
-        else:
-            flag = False
 
-
-    # flag remains false when specified tag not found in files
-    if flag is False:
+    if tagName not in usedTags:
         root.remove(LocaleResource)
         removedTags.append(tagName)
         print(str(counter) + ' : ' + tagName + ' Removed')
     else:
         foundTags.append(tagName + ' exists in  : ' + file)
-        print(str(counter) + ' : ' + tagName + ' exists in : '+ file)
+        print(str(counter) + ' : ' + tagName + ' exists in : ' + file)
 
+allTags = [
+    e.attrib['Name'] for e in tree.iter()
+    if 'Name' in e.attrib and e.tag == 'LocaleResource'
+]
 
-
+# removeList = [tag for tag in allTags if tag not in usedTags]
+# ...
 
 # generate new modified xml file in XML_Modifier.py directory
-tree.write('Output.xml',encoding='utf-8')
+tree.write('Output.xml', encoding='utf-8')
 
 # make new text file that contains list of removed tags of xml file
 RemovedTagsFile = open('RemovedTags.txt', 'a')
 RemovedTagsFile.write(str(len(removedTags)) + ' tags removed : \n')
 RemovedTagsFile.write(listToString(removedTags))
 RemovedTagsFile.close()
-
 
 # make new text file that contains list of found tags of xml file
 FoundTagsFile = open('FoundTags.txt', 'a')
